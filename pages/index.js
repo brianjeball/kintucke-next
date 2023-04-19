ad1e15a72f424fee242ce72f7885764ad8d30de4
Brian J. Ball <41380090+brianjeball@users.noreply.github.com>
2023-04-19 02:36:40 +0000

    add cover art, update homepage, styles updated

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const CoverArtInfoModal = () => {
       <Popover.Button style={{ backgroundColor: 'white', width: '25px', borderRadius: '25px', fontWeight: 900 }}>i</Popover.Button>
 
       <Popover.Panel className="absolute z-10">
-        <Image src="/assets/images/main_cover_art2.jpeg" alt='catch22 cover art' width={300} height={300} />
+        <Image src="/assets/images/kintucke-cover.png" alt='catch22 cover art' width={300} height={300} />
         <p style={{ width: '300px', padding: '20px', textAlign: 'center', backgroundColor: 'whitesmoke', bottom: '20%', left: '-300px' }}>Select each circle to complete the Cover Art</p>
       </Popover.Panel>
     </Popover>
@@ -25,7 +25,9 @@ const Home = () => {
     <Layout pageTitle="home">
       <Header />
       <CoverArtInfoModal />
-      <CoverArtInteractive />
+
+      {/* <CoverArtInteractive /> */}
+      <Image src="/assets/images/kintucke-cover.png" alt='catch22 cover art' width={600} height={600} />
       <Footer />
     </Layout>
   )
